709851096e2551af1d81e1e87fab4187ca70c8df
Viictor-gulielmitti <79719548+Viictor-gulielmitti@users.noreply.github.com>
2021-03-06 17:51:26 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+             Console.ForegroundColor = ConsoleColor.Green;            
+            Console.WriteLine("Música: Mc paulin da capital - Acredita");
+             Console.ForegroundColor = ConsoleColor.White;            
+            Console.WriteLine("");
+            Console.WriteLine("***************************************");
+            Console.WriteLine("");
+             Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("É quando te falarem que você não vai vencer");
+            Console.WriteLine("Pode ter certeza que isso tudo é mentira");
+            Console.WriteLine("Deposite a fé no poderoso pra tu ver");
+            Console.WriteLine("Que ele orienta é abençoa a tua vida");
+            Console.WriteLine("Se tiver perdido é só você ligar pra ele");
+            Console.WriteLine("Parece que não, mas ele sempre tá te ouvindo");
+            Console.WriteLine("Tá ouvindo a preces desses maloqueiros");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Acredita naquele que sempre te deu a mão");
+            Console.WriteLine("Naquele que sempre deu proteção, Senhor");
+            Console.WriteLine("Acredita naquele que sempre te deu valor");
+            Console.WriteLine("Naquele que sempre te ajudou, Senhor");
+            Console.WriteLine("É acredita, ah ah");
+            Console.WriteLine("Acredita naquele que sempre te deu a mão");
+            Console.WriteLine("Naquele que sempre deu proteção, Senhor");
+            Console.WriteLine("Acredita naquele que sempre te deu valor");
+            Console.WriteLine("Naquele que sempre te ajudou, Senhor");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("É quando te falarem que você não vai vencer");
+            Console.WriteLine("Pode ter certeza que isso tudo é mentira");
+            Console.WriteLine("Deposite a fé no poderoso pra tu ver");
+            Console.WriteLine("Que ele orienta é abençoa a tua vida");
+            Console.WriteLine("Se tiver perdido é só você ligar pra ele");
+            Console.WriteLine("Parece que não, mas ele sempre tá te ouvindo");
+            Console.WriteLine("Tá ouvindo a preces desses maloqueiros");
+            Console.ForegroundColor =ConsoleColor.White;
+            Console.WriteLine("Acredita naquele que sempre te deu a mão");
+            Console.WriteLine("Naquele que sempre deu proteção, Senhor");
+            Console.WriteLine("Acredita naquele que sempre te deu valor");
+            Console.WriteLine("Naquele que sempre te ajudou, Senhor");
+            Console.WriteLine("É acredita");
+            Console.WriteLine("Acredita naquele que sempre te deu a mão");
+            Console.WriteLine("Naquele que sempre deu proteção, Senhor");
+            Console.WriteLine("Acredita naquele que sempre te deu valor");
+            Console.WriteLine("Naquele que sempre te ajudou, Senhor");
+            Console.WriteLine("É acredita, ah ah");
+           
+
+        }
+    }
+}
